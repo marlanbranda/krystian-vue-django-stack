f50097c0a811a516b2be950b164d491a370a48bb
Emre Kızılırmak <emrek1999@gmail.com>
2023-06-21 01:36:27 +0300

    trials with the field types

--- back_kyrilic/demo/models.py
+++ back_kyrilic/demo/models.py
@@ -3,3 +3,8 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
+    description = models.TextField(max_length=500, blank=True)
+    price = models.DecimalField(default=0, max_digits=4, decimal_places=2)
+    published = models.DateField(auto_now=True, auto_now_add=True)
+    cover = models.FileField(upload_to="covers/")
+    images = models.ImageField(upload_to="covers/", height_field=400, width_field=400)
