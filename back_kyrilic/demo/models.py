d659adc3f2d7922747537f6770b2d7f731e046df
Emre Kızılırmak <emrek1999@gmail.com>
2023-06-21 01:40:31 +0300

    migrations and little modifications with model Book

--- back_kyrilic/demo/models.py
+++ back_kyrilic/demo/models.py
@@ -5,6 +5,7 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=500, blank=True)
     price = models.DecimalField(default=0, max_digits=4, decimal_places=2)
-    published = models.DateField(auto_now=True, auto_now_add=True)
-    cover = models.FileField(upload_to="covers/")
-    images = models.ImageField(upload_to="covers/", height_field=400, width_field=400)
+    published = models.DateField(auto_now=True)
+    cover = models.FileField(upload_to="covers/", default='settings.MEDIA_ROOT/logos/anonymous.jpg')
+    images = models.ImageField(upload_to="covers/", height_field=400, width_field=400,
+                               default='settings.MEDIA_ROOT/logos/anonymous.jpg')
