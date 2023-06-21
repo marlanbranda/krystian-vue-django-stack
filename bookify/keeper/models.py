dd4c0b629a428f100c16808a28e4f593b8825323
Emre Kızılırmak <emrek1999@gmail.com>
2023-06-21 23:09:54 +0300

    created the model Book

--- bookify/keeper/models.py
+++ bookify/keeper/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=50, null=False)
+    description = models.TextField(max_length=500, null=False)
+    price = models.DecimalField(max_digits=4, decimal_places=2, default=0)
+    creation_date = models.DateField(auto_now_add=True)
+    owned = models.BooleanField(default=False)
+    cover = models.FileField(upload_to="image/")
+
+    def __str__(self) -> str:
+        return self.title 
