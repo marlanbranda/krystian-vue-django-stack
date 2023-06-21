452cc0548c4cb9e5d74499f0ff2d05567f8ebe93
Emre Kızılırmak <emrek1999@gmail.com>
2023-06-22 00:24:22 +0300

    admin customisation by ModelAdmin sub-class

--- bookify/keeper/admin.py
+++ bookify/keeper/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Book
 
-
-admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # fields = ['title', 'description'] # this is lets you to limit the admin to certain fields in model 
+    list_display = ['title', 'description', 'price']
+    list_filter = ['owned']
+    search_fields = ['description']
