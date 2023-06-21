6b0470ec6f906d8e9a1ca43e477964fa19efa62d
Emre Kızılırmak <emrek1999@gmail.com>
2023-06-21 16:08:24 +0300

    add model to str represantation

--- back_kyrilic/demo/models.py
+++ back_kyrilic/demo/models.py
@@ -9,3 +9,6 @@ class Book(models.Model):
     price = models.DecimalField(default=0, max_digits=4, decimal_places=2)
 
     published = models.DateField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
