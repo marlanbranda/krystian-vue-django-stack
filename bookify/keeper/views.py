f55aef5fc0e9a89d0dcaa52f4fda766bd481ba4e
Emre Kızılırmak <emrek1999@gmail.com>
2023-06-22 02:39:52 +0300

    tokens with restframework

--- bookify/keeper/views.py
+++ bookify/keeper/views.py
@@ -5,11 +5,12 @@ from django.shortcuts import render
 
 from rest_framework import viewsets
 from .serializers import BookSerializer
-
+from rest_framework.authentication import TokenAuthentication
 
 class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
+    authentication_classes = (TokenAuthentication, )
 
 
 
