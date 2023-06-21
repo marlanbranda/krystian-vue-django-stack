3ed3f8be0418fc1fcee71113cae9ddc9e568158b
Emre Kızılırmak <emrek1999@gmail.com>
2023-06-22 01:47:43 +0300

    serializer, router and viewset from restframework

--- bookify/keeper/urls.py
+++ bookify/keeper/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import function_view, ClassView, template_view
+from .views import function_view, ClassView, template_view, BookViewSet
+
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('books', BookViewSet)
+
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('function/', function_view),
     path('class/', ClassView.as_view()),
     path('template/', template_view),
--- bookify/keeper/views.py
+++ bookify/keeper/views.py
@@ -3,13 +3,21 @@ from django.views import View
 from .models import Book
 from django.shortcuts import render
 
+from rest_framework import viewsets
+from .serializers import BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+
 
 def template_view(request):
     
     books = Book.objects.all()
 
     return render(request, 'first_template.html', {'books': books})
-
 
 
 class ClassView(View):
