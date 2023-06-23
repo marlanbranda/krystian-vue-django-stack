b7598461bbcf31d8d952245ed481baf2172f48c5
Emre Kızılırmak <emrek1999@gmail.com>
2023-06-23 03:32:21 +0300

    added bookisbn serializer

--- bookify/keeper/serializers.py
+++ bookify/keeper/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
-from .models import Book
+from .models import Book, BookISBN
+
+
+class BookISBNSerialized(serializers.ModelSerializer):
+    class Meta:
+        model = BookISBN
+        fields = ['isbn_10', 'isbn_13']
+
 
 class BookSerializer(serializers.ModelSerializer):
+    isbn = BookISBNSerialized(many=False)
+    
     class Meta:
         model = Book
-        fields = ['id', 'title', 'description', 'price', 'owned']
+        fields = ['id', 'title', 'description', 'price', 'owned', 'isbn']
